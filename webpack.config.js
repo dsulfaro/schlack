def9714b1cfe1a197a5f786a3a9dcaf552167dc8
Dan Sulfaro <dsulfaro1@gmail.com>
2016-12-23 20:47:40 -0500

    finish initial webpack config -- make sure you're in the root directory when running webpack -_-

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,14 @@
-var path = require("path");
-
 module.exports = {
-  context: __dirname,
-  entry: "./frontend/entry.jsx",
+  entry: './frontend/entry.jsx',
   output: {
-    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
-    filename: "bundle.js"
+    path: 'app/assets/javascripts',
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
       {
         test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         loader: 'babel',
         query: {
           presets: ['es2015', 'react']
@@ -19,8 +16,8 @@ module.exports = {
       }
     ]
   },
-  devtool: 'source-maps',
+  devtool: 'source-map',
   resolve: {
-    extensions: ["", ".js", ".jsx" ]
+    extensions: ['', '.js', '.jsx' ]
   }
 };
