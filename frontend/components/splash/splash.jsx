2690a2e1e38c6cb5fa4403a9bc6d7dca9e2c402d
Dan Sulfaro <dsulfaro1@gmail.com>
2016-12-27 18:29:14 -0500

    begin auth forms for splash container

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import Modal from 'react-modal';
 
 class Splash extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      siOpen: false,
+    }
+    this.open_sign_up_form = this.open_sign_up_form.bind(this);
+    this.close_sign_up_form = this.close_sign_up_form.bind(this);
+    this.render_sign_up_form = this.render_sign_up_form.bind(this);
+  }
+
+  componentWillMount() {
+    Modal.setAppElement('body');
+  }
+
+  open_sign_up_form() {
+    this.setState({siOpen: true});
+  }
+
+  close_sign_up_form() {
+    this.setState({siOpen: false});
+  }
+
+  render_sign_up_form() {
+    return (
+      <Modal
+        isOpen={this.state.siOpen}
+        contentLabel="Sign Up Form"
+        >
+        <form id="sign-up-form">
+          <h1 onClick={this.close_sign_up_form}>Hello</h1>
+          <input type="text" placeholder="First Name"></input>
+          <input type="text" placeholder="Last Name"></input>
+          <input type="text" placeholder="Username"></input>
+          <input type="text" placeholder="Password"></input>
+        </form>
+      </Modal>
+    )
+  }
+
   render_header() {
     return (
-      <header  id="splash-header">
+      <header id="splash-header">
         <h1>shlack</h1>
         <ul>
           <li>Product</li>
@@ -19,7 +59,11 @@ class Splash extends React.Component {
   }
 
   render_footer() {
-
+    return (
+      <footer id="splash-footer">
+        <p id="splash-sign-up" onClick={this.open_sign_up_form}>Sign Up</p>
+      </footer>
+    )
   }
 
   render() {
@@ -27,6 +71,8 @@ class Splash extends React.Component {
       <div id="splash">
         { this.render_header() }
         <h1 id="test">Shlack</h1>
+        { this.render_sign_up_form() }
+        { this.render_footer() }
       </div>
     )
   }
